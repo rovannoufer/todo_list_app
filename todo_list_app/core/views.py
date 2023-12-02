3cea81d37064ae9dd8fda66d06c7ef986461cc36
rovannoufer <noufermohamed007@gmail.com>
2023-12-03 01:17:25 +0530

    class based view

--- todo_list_app/core/views.py
+++ todo_list_app/core/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from . models import Task
 
+class Task_List(ListView):
+    model = Task
+    context_object_name = 'tasks'
 
+class Task_Detail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'core/task.html'
+
+class Task_Create(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('tasks')
