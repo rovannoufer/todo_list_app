4e5ffe3b77b87f7256f1b04b2acb12ae3834bb32
rovannoufer <noufermohamed007@gmail.com>
2023-12-03 01:16:25 +0530

    Updated

--- todo_list_app/core/urls.py
+++ todo_list_app/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import Task_List, Task_Detail, Task_Create
 
 
 urlpatterns = [
-    path('', views.task_list, name="tasks")
+    path('', Task_List.as_view(), name="tasks"),
+    path('task/<int:pk>/', Task_Detail.as_view(), name='task'),
+    path('task-create/', Task_Create.as_view(), name='task-create')
 ]
