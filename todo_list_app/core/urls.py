6df6e958a8063a7062a774558a14412535517f49
rovannoufer <noufermohamed007@gmail.com>
2023-12-03 16:15:47 +0530

    almost finish

--- todo_list_app/core/urls.py
+++ todo_list_app/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Task_List, Task_Detail, Task_Create, Task_Update, Task_Delete, Custom_Login
+from .views import Task_List, Task_Detail, Task_Create, Task_Update, Task_Delete, Custom_Login, Signup_Page
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('task-create/', Task_Create.as_view(), name='task-create'),
     path('task-update/<int:pk>/', Task_Update.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', Task_Delete.as_view(), name='task-delete'),
-      path('login/', Custom_Login.as_view(), name="login"),
-      path('logout/', LogoutView.as_view(next_page = 'login'), name="logout")
+    path('login/', Custom_Login.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name="logout"),
+    path('signup/', Signup_Page.as_view(), name='signup')
 ]
--- todo_list_app/core/views.py
+++ todo_list_app/core/views.py
@@ -1,32 +1,54 @@
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from . models import Task
 
-class Task_List(ListView):
+class Task_List(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-class Task_Detail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+        
+        context['search_input'] = search_input
+        return context
+
+
+class Task_Detail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'core/task.html'
 
-class Task_Create(CreateView):
+class Task_Create(LoginRequiredMixin, CreateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class Task_Update(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(Task_Create, self).form_valid(form)
+
+class Task_Update(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
-class Task_Delete(DeleteView):
+class Task_Delete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
@@ -39,3 +61,22 @@ class Custom_Login(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+
+class Signup_Page(FormView):
+    template_name = 'core/signup.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form): 
+      user = form.save()
+      if user is not None:
+          login(self.request, user)
+      return super(Signup_Page, self).form_valid(form)
+    
+    # overriding the signup page with login user 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(Signup_Page, self).get(*args, **kwargs)
